42351a0ccc79cf4de514671a95d01b74cc0dc193
luchthrash <elucatel@gmail.com>
2017-10-16 17:49:56 +0000

    wwwwwwwwwwwww

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+/**
+ * @param
+ */
+  public final String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,5 @@
+
+/**
+ * culsossssss
+ */
+package com.example;
